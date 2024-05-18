80b5a577798591673006ef3a50450d0ee9a23d6b
soramimi <fi7s-fct@asahi-net.or.jp>
2024-05-18 14:02:33 +0900

    Add new parsing functions for main data structures

--- jstream.h
+++ jstream.h
@@ -6,6 +6,7 @@
 #include <functional>
 #include <string>
 #include <vector>
+#include <map>
 
 namespace jstream {
 
@@ -505,9 +506,9 @@ public:
 		return path + d.key;
 	}
 
-	bool match(char const *path, std::vector<std::string> *vals = nullptr) const
+	bool match(char const *path, std::vector<std::string> *vals = nullptr, bool clear = true) const
 	{
-		if (vals) {
+		if (vals && clear) {
 			vals->clear();
 		}
 		if (!(isobject() || isvalue())) return false;
@@ -516,10 +517,10 @@ public:
 			std::string const &s = d.depth[i];
 			if (s.empty()) break;
 			if (path[0] == '*' && (path[1] == 0 || path[1] == '{') && s.c_str()[s.size() - 1] == '{') {
-				std::string t;
 				if (path[1] == 0) {
 					if (i + 1 == d.depth.size()) {
 						if (vals) {
+							std::string t;
 							while (i < d.depth.size()) {
 								t += d.depth[i];
 								i++;
@@ -532,10 +533,6 @@ public:
 						return true;
 					}
 					return false;
-				}
-				t = s.substr(0, s.size() - 1);
-				if (vals) {
-					vals->push_back(t);
 				}
 				path += 2;
 				continue;
@@ -556,7 +553,42 @@ public:
 			}
 			return false;
 		}
-		return path == d.key;
+		if (path == d.key) {
+			if (vals && isvalue()) {
+				vals->push_back(string());
+			}
+			return true;
+		}
+		return false;
+	}
+
+	typedef std::map<std::string_view, std::string *> rule_for_string_t;
+	typedef std::map<std::string_view, std::vector<std::string> *> rule_for_strings_t;
+
+	void parse(rule_for_string_t const &rule, char sep = ',')
+	{
+		while (next()) {
+			for (auto &t : rule) {
+				std::vector<std::string> vals;
+				if (match(t.first.data(), &vals, true)) {
+					for (auto const &s : vals) {
+						if (!t.second->empty()) {
+							*t.second += sep;
+						}
+						*t.second += s;
+					}
+				}
+			}
+		}
+	}
+
+	void parse(rule_for_strings_t const &rule)
+	{
+		while (next()) {
+			for (auto &t : rule) {
+				match(t.first.data(), t.second, false);
+			}
+		}
 	}
 };
 
--- main.cpp
+++ main.cpp
@@ -2,6 +2,9 @@
 #include "jstream.h"
 #include "test.h"
 #include <stdio.h>
+#include <string_view>
+#include <map>
+#include <iostream>
 
 struct GoogleAccessToken {
 	std::string access_token;
@@ -10,9 +13,34 @@ struct GoogleAccessToken {
 	std::string scope;
 	std::string refresh_token;
 	std::string id_token;
+
+	std::string stringify() const
+	{
+		std::string ret;
+
+		jstream::Writer w([&](char const *p, int n){
+			ret += {p, n};
+		});
+
+		w.object({}, [&](){
+			w.string(access_token, "access_token");
+			w.string(expires_in, "expires_in");
+			w.string(scope, "scope");
+			w.string(token_type, "token_type");
+			w.string(id_token, "id_token");
+		});
+
+		return ret;
+	}
 };
 
-GoogleAccessToken parse()
+std::ostream &operator << (std::ostream &out, GoogleAccessToken const &token)
+{
+	out << token.stringify();
+	return out;
+}
+
+GoogleAccessToken parse_google_access_token()
 {
 	std::string input = R"---(
 {
@@ -42,19 +70,29 @@ GoogleAccessToken parse()
 	return out;
 }
 
-void print(GoogleAccessToken const &token)
+GoogleAccessToken parse_google_access_token2()
 {
-	jstream::Writer w([](char const *p, int n){
-		fwrite(p, 1, n, stdout);
-	});
+	std::string input = R"---(
+{
+"access_token": "qwerty123",
+"expires_in": 3599,
+"scope": "https://www.googleapis.com/auth/userinfo.profile",
+"token_type": "Bearer",
+"id_token": "abcdefg"
+}
+)---";
 
-	w.object({}, [&](){
-		w.string(token.access_token, "access_token");
-		w.string(token.expires_in, "expires_in");
-		w.string(token.scope, "scope");
-		w.string(token.token_type, "token_type");
-		w.string(token.id_token, "id_token");
-	});
+	GoogleAccessToken out;
+	jstream::Reader::rule_for_string_t rule;
+	jstream::Reader json(input.c_str(), input.size());
+	rule["{access_token"] = &out.access_token;
+	rule["{expires_in"] = &out.expires_in;
+	rule["{scope"] = &out.scope;
+	rule["{token_type"] = &out.token_type;
+	rule["{id_token"] = &out.id_token;
+	json.parse(rule);
+
+	return out;
 }
 
 void main2()
@@ -63,7 +101,11 @@ void main2()
 {
   "items": {
 	"hoge": {
-	  "name": "foo"
+	  "name": [
+        "aaa",
+        "bbb",
+        "ccc"
+      ]
 	},
 	"fuga": {
 	  "name": "bar"
@@ -72,24 +114,105 @@ void main2()
 }
 )---";
 
+	std::vector<std::string> hogenames;
+	std::vector<std::string> fuganames;
+	jstream::Reader::rule_for_strings_t rule;
+	rule["{items{hoge{name["] = &hogenames;
+	rule["{items{fuga{name"] = &fuganames;
+
 	jstream::Reader json(input.c_str(), input.size());
-	while (json.next()) {
-		std::vector<std::string> vals;
-		if (json.match("{items{*{name", &vals)) {
-			puts(vals[0].c_str());
-		}
+	json.parse(rule);
+
+	for (auto const &s : hogenames) {
+		puts(s.c_str());
 	}
+
+	for (auto const &s : fuganames) {
+		puts(s.c_str());
+	}
+
+}
+
+void main3()
+{
+	std::string input = R"---(
+{
+  "items": {
+	"hoge": {
+	  "name": [
+        "aaa",
+        "bbb",
+        "ccc"
+      ]
+	},
+	"fuga": {
+	  "name": "bar"
+	}
+  }
+}
+)---";
+
+	std::vector<std::string> hogenames;
+	std::vector<std::string> fuganames;
+	jstream::Reader::rule_for_strings_t rule;
+	rule["{items{*{name["] = &hogenames;
+	rule["{items{*{name"] = &fuganames;
+
+	jstream::Reader json(input.c_str(), input.size());
+	json.parse(rule);
+
+	for (auto const &s : hogenames) {
+		puts(s.c_str());
+	}
+
+	for (auto const &s : fuganames) {
+		puts(s.c_str());
+	}
+
+}
+
+void main4()
+{
+	std::string input = R"---(
+{
+  "items": {
+	"hoge": {
+	  "name": [
+        "aaa",
+        "bbb",
+        "ccc"
+      ]
+	},
+	"fuga": {
+	  "name": "bar"
+	}
+  }
+}
+)---";
+
+	std::string hogename;
+	std::string fuganame;
+	jstream::Reader::rule_for_string_t rule;
+	rule["{items{hoge{name["] = &hogename;
+	rule["{items{fuga{name"] = &fuganame;
+
+	jstream::Reader json(input.c_str(), input.size());
+	json.parse(rule);
+
+	puts(hogename.c_str());
+	puts(fuganame.c_str());
+
 }
 
 int main()
 {
 #if 0
-	main2();
-#elif 1
+	main4();
+#elif 0
 	test_all();
 #else
-	auto t = parse();
-	print(t);
+	auto t = parse_google_access_token();
+	std::cout << t;
 #endif
 
 	return 0;
